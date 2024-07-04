f81a8c1159af09151f834bdab3324d4ced788bfd
bearcubbb <rafandsulfus@gmail.com>
2024-07-04 04:07:59 +0400

    diplomas+slider-2

--- assets/scripts/slider.js
+++ assets/scripts/slider.js
@@ -18,50 +18,7 @@ backBtn.addEventListener("click", () => {
     scrollContainer.scrollLeft -= 400;
 })
 
-/* let currentSlide = 0;
-const slides = document.querySelectorAll('.photos img');
-const totalSlides = slides.length;
-
-function showSlide(index) {
-    slides.forEach((slide, i) => {
-        if (i == index || i == index + 1) {
-            slide.style.display = 'block';
-        } else {
-            slide.style.display = 'none';
-        }
-    });
+/*function signUp {
+    document.location = 'page/new.html'
 }
-
-document.querySelector('.next').addEventListener('click', () => {
-    currentSlide = (currentSlide + 2) % totalSlides;
-    showSlide(currentSlide);
-});
-
-document.querySelector('.prev').addEventListener('click', () => {
-    currentSlide = (currentSlide - 2 + totalSlides) % totalSlides;
-    showSlide(currentSlide);
-});
-
-showSlide(currentSlide);
-*/
-
-/*new Splide(".splide", {
-    type: "fade",
-    pagination: false,
-    rewind: true,
-    autoplay: true,
-    breakpoints: {
-        897: {
-            type: "slide",
-            rewind: false,
-            perPage: 2,
-        },
-    },
-    classes: {
-        arrows: "splide__arrows arrows",
-        arrow: "splide__arrow arrow",
-        prev: "splide__arrow--prev l-bts",
-        next: "splide__arrow--next r-bts",
-    },
-}).mount();
-*/
+    */
