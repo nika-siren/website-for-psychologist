03a969db32f3a2d96181e6fe31fdf14b2877583d
nika-siren <rafandsulfus@gmail.com>
2024-07-30 23:40:01 +0400

    smooth-scroll-full

--- assets/scripts/slider.js
+++ assets/scripts/slider.js
@@ -38,3 +38,26 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 });
+
+/* smooth scroll */
+
+window.addEventListener("DOMContentLoaded", () => {
+    const smoothScroll = () => {
+        const links = document.querySelectorAll(".menu-link");
+
+        links.forEach((link) => {
+            link.addEventListener("click", (event) => {
+                event.preventDefault();
+                const section = document.querySelector(link.getAttribute("href"));
+
+                section.scrollIntoView({
+                    behavior: "smooth",
+                    block: "start",
+                    inline: "center",
+                });
+            });
+        });
+    };
+
+    smoothScroll();
+})
